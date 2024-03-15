0381849c07fb3611dc84544393da57d62273ab2e
bordlampe123 <104758251+bordlampe123@users.noreply.github.com>
2024-03-15 08:20:42 +0100

    pis

--- Benjamins Mappe/Histogram_plot.py
+++ Benjamins Mappe/Histogram_plot.py
@@ -8,22 +8,22 @@ import matplotlib.pyplot as plt
 #Try different numbers of bins. Is the data approximately normal?
 
 
+def opgave_1():
+    #Starlink_data = pd.read_csv("Starlink_latency.csv")
+    df = pd.read_csv("temperature.csv")
+    print(df.keys())
 
-#Starlink_data = pd.read_csv("Starlink_latency.csv")
-df = pd.read_csv("temperature.csv")
-print(df.keys())
+    #absolut
+    plt.hist(df[" 26.7"], bins=10, color="red")
 
-#absolut
-plt.hist(df[" 26.7"], bins=10, color="red")
+    #Normalized
+    plt.hist(df[" 26.7"], bins=10, color="blue", density=True)
 
-#Normalized
-plt.hist(df[" 26.7"], bins=10, color="blue", density=True)
+    #Relative - Denne er meget mindre i værdi og læser derfor ikke så godt når de andre værdier også bliver plottet
+    plt.hist(df[" 26.7"], bins=10, color="black", weights=np.ones_like(df[" 26.7"]) / len(df[" 26.7"]))
 
-#Relative - Denne er meget mindre i værdi og læser derfor ikke så godt når de andre værdier også bliver plottet
-plt.hist(df[" 26.7"], bins=10, color="black", weights=np.ones_like(df[" 26.7"]) / len(df[" 26.7"]))
-
-#df.plot.hist()
-plt.show()
+    #df.plot.hist()
+    plt.show()
 
 
 
@@ -39,18 +39,56 @@ Terning = np.random.randint(1,7, 100)
 
 
 
+def opgave_2():
+    df2 = pd.DataFrame({"die": np.random.randint(1,7,100)})
+    df2_empty = []
+    df2_mean = []
+    df2_x = []
+    #print(df.mean())
+    print(len(df2))
 
-df2 = pd.DataFrame({"die": np.random.randint(1,7,100)})
-df2_empty = []
-df2_mean = []
-#print(df.mean())
-print(len(df2))
+    for i in range(len(df2)):
+        df2_empty.append(df2["die"][i])
+        df2_mean.append(np.mean(df2_empty))
+        df2_x.append(i)
+        
 
-for i in range(len(df2)):
-    df2_empty.append(df2["die"][i])
-    df2_mean.append(np.mean(df2_empty))
-    
-print(df2_mean)
+    plt.plot(df2_x, df2_mean, color ="green") 
+    plt.show()
+
+
+
+
+
+#Exercise 3: Consider the case of flipping a fair coin n times and let Hn be the random variable (RV) of the number of heads.
+#Also recall that the sum of n Bernoulli RVs is a Binomial random variable with mean np and variance np(2−p).
+#Calculate and compare the probability of observing H10 ≥ 7 and H100 ≥ 70 using each of the following methods.
+#a) Summing over the formula for the probability mass function (pmf) of Binomial RVs (Bliver ikke lavet, det er vidst mere sandsynlighed)
+#b) Using the central limit theorem
+
+
+#Når man skal bruge the central limit theorom skal man finde den normaliserede mean
+coin = pd.DataFrame({"heads":np.random.randint(0,2,100)})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
