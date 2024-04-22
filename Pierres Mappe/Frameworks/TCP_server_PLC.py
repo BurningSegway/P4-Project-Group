e9cc3165f6af7e1774d78b670d44fd4a0c1ed36b
BurningSegway <urkanin@gmail.com>
2024-04-22 14:49:08 +0200

    CoDeSys og python - wow

---
+++ Pierres Mappe/Frameworks/TCP_server_PLC.py
@@ -0,0 +1,20 @@
+import socket
+
+HOST = "172.20.66.47"  # Standard loopback interface address (localhost)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    inst = 2
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            data_corrected = data.split(b'.')
+            print(data_corrected)
+            print(f"Received from PLC: {data_corrected[0]}")
+            if not data:
+                break
+            conn.sendall(b'2')
